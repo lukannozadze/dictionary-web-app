0460deb7eb57ec33ba273058bcf452301aac30b2
Luka <nozadzeluka2002@gmail.com>
2023-03-17 23:38:16 +0400

    created: search input response to errors

--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -1,45 +1,46 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import SearchIcon from "../UI/SearchIcon";
 import { useForm } from "react-hook-form";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
 const SearchBar = () => {
   const schema = yup.object().shape({
-    word: yup.string().required("Whoops, can’t be empty…"),
+    word: yup
+      .string()
+      .required("Whoops, can’t be empty…")
+      .matches(/^[aA-zZ]+$/, "Wrong Format!"),
   });
   const {
     register,
     handleSubmit,
+    watch,
     formState: { errors },
   } = useForm({
     resolver: yupResolver(schema),
   });
-  console.log(errors.word.message);
+
   const [word, setWord] = useState("");
+  useEffect(() => {
+    setWord(watch("word"));
+  }, [watch("word")]);
   return (
-    <form onSubmit={handleSubmit((e) => e.preventDefault())} id="form">
+    <form onSubmit={handleSubmit((e) => e.preventDefault)} id="form">
       <label className="relative" htmlFor="searchWord">
         <input
           {...register("word")}
-          className={`w-full h-12 ${
-            errors.word !== undefined ? "border-red-500" : ""
-          } bg-[#F4F4F4] pl-6 rounded-2xl outline-none text-4 placeholder:font-bold font-bold text-[#2D2D2D]  `}
+          className={`w-full h-12 bg-[#F4F4F4] ${
+            errors.word ? "border-[1px] border-[#FF5252]" : ""
+          } pl-6 rounded-2xl outline-none text-4 placeholder:font-bold font-bold text-[#2D2D2D]`}
           type="text"
-          name="searchWord"
           placeholder="Search for any word..."
-          onChange={(e) => {
-            setWord(e.target.value);
-          }}
         />
         <button form="form">
-          <SearchIcon
-            className="absolute top-1 right-6 "
-            searchedWord={word}
-            handleSubmit={handleSubmit}
-          />
+          <SearchIcon className="absolute top-1 right-6 " searchedWord={word} />
         </button>
       </label>
-      <span>{errors.word?.message}</span>
+      <span className="text-[#FF5252] text-4 leading-5">
+        {errors.word?.message}
+      </span>
     </form>
   );
 };
--- src/UI/PlayerIcon.jsx
+++ src/UI/PlayerIcon.jsx
@@ -1,36 +1,27 @@
 import { useEffect, useState } from "react";
 
 const PlayerIcon = (props) => {
-  console.log(props);
   const [audioPath, setAudioPath] = useState("");
   const [audioObj, setAudioObj] = useState({});
   useEffect(() => {
     if (Object.keys(props.wordObj).length !== 0) {
-      console.log("level1");
       if (props.wordObj[0].phonetics.length > 0) {
-        console.log("level2");
         props.wordObj[0].phonetics.forEach((el) => {
           if (el.audio !== "") {
             setAudioPath(el.audio);
-            console.log(el.audio);
           }
         });
       } else {
         setAudioPath(props.wordObj[0].phonetics[0]);
-        console.log(props.wordObj[0].phonetics[0]);
-        console.log("test");
       }
     }
   }, [props.wordObj]);
-  console.log(props);
   useEffect(() => {
     let audio = new Audio(audioPath);
     setAudioObj(audio);
-    console.log(audioObj);
   }, [audioPath]);
   const playAudio = () => {
     if (audioObj) {
-      console.log("level3");
       audioObj.play();
     }
   };
